e1ec41e1dc40b62d392e103d05ebb9502625c662
Konstantin <70294424+ElTabaco@users.noreply.github.com>
2025-01-22 22:40:08 +0100

    Update delete.sh

--- kubernetes/delete.sh
+++ kubernetes/delete.sh
@@ -2,4 +2,6 @@ kubectl delete -f mr-do-openhab-deployment.yml
 kubectl delete -f mr-do-openhab-services.yml
 kubectl delete -f mr-do-openhab-pvc.yml
 kubectl delete -f mr-do-openhab-pv.yml
+
+kubectl delete all,deployment,pv,pvc --all -n mr-do-openhab
 kubectl delete namespace mr-do-openhab
